6635b1a4e4bcc0dca94a837195a3a6b43e48463b
Anisha <anishamohanty23@gmail.com>
2020-06-09 23:08:53 +0530

    modified query

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,42 +1,41 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.*;
 
-
-@SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) throws SQLException {
+        SpringApplication.run(DemoApplication.class, args);
 
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		CustomerDAO customerDAO = context.getBean(CustomerDAO.class);
+        CustomerDAO customerDAO = context.getBean(CustomerDAO.class);
 
-		for( Customer c : customerDAO.listCustomer()) {
-			System.out.println(c);
-		}
+        for (Customer c : customerDAO.listCustomer()) {
+            System.out.println(c);
+        }
 
-		System.out.println("Creating New Customer : ");
-		Customer customer = new Customer(13, "Augustus Mir", "CST1005");
-		System.out.println(customer);
-		customerDAO.createCustomer(customer);
+        System.out.println("Creating New Customer : ");
+        Customer customer = new Customer(13, "Augustus Mir", "CST1005");
+        System.out.println(customer);
+        customerDAO.createCustomer(customer);
 
-		Customer c1 = customerDAO.getCustomerById((long)11);
-		c1.setName("Hanna Jones");
+        Customer c1 = customerDAO.getCustomerById((long) 11);
+        c1.setName("Hanna Jones");
 
-		System.out.println("Updating existing Customer : ");
-		customerDAO.updateCustomer(c1);
+        System.out.println("Updating existing Customer : ");
+        customerDAO.updateCustomer(c1);
 
-		for( Customer c : customerDAO.listCustomer()) {
-			System.out.println(c);
-		}
-	}
+        for (Customer c : customerDAO.listCustomer()) {
+            System.out.println(c);
+        }
+    }
+}
 
-//
 //	@Override
 //	public void run(String... args) throws Exception {
 //		String SQL_GET_ALL = "SELECT * FROM CUSTOMER";
@@ -49,5 +48,5 @@ public class DemoApplication {
 //				.query("SELECT * FROM CUSTOMER WHERE ID=10", new CustomerMapper())
 //				.forEach(s-> System.out.println(s.getId() +" " + s.getName() + " " + s.getSsn()));
 //    }
-}
+
 
